5f278214d4ffcfdec66d20137ea4805c8e336f74
WyEhNf <1716096609@qq.com>
2025-11-25 16:43:54 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,37 +5,53 @@
 #include "../include/Financesys.hpp"
 #include <string>
 #include <iostream>
-
+#include <cstdio>
 std::vector<std::pair<chars,string> > LOG;
 int main()
 {
+    std::freopen("../read.in","r",stdin);
+    std::freopen("../report.txt","w",stdout);
+    // std::freopen("write.out","w",stdout);
     int batch=0;
+    std::cerr<<"BookStore Launched\n";
     while(!std::cin.eof())
     {
         ++batch;
+        std::cout<<batch<<' ';
         try{
             std::string st;
-            std::cin>>st;
+            getline(std::cin,st);
             string stcop=st;
             Read a(st);
             if(!a.length()) continue;
             string op=a.get_string();
+            std::cerr<<op<<'\n';
             if(op=="quit"||op=="exit") return 0;
             else if(op=="su")
             {
                 Stack.Login(a);
+                std::cerr<<"Done.\n";
             }else if(op=="logout")
             {
                 if(a.length()) throw 0;
                 Stack.Logout();
+                std::cerr<<"Done.\n";
             }else if(op=="passwd"){
                 Stack.Password(a);
-            }else if(op=="useradd")
+                std::cerr<<"Done.\n";
+            }else if(op=="register")
+            {
+                Stack.Register(a);
+                std::cerr<<"Done.\n";
+            }
+            else if(op=="useradd")
             {
                 Stack.Create(a);
+                std::cerr<<"Done.\n";
             }else if(op=="delete")
             {
                 Stack.Delete(a);
+                std::cerr<<"Done.\n";
             }else if(op=="show")
             {
                 Read cop=a;
@@ -44,22 +60,31 @@ int main()
                     string tmp=a.get_string();
                     Finance_Record.print(a);
                 }else BookManager.show(a);
+                std::cerr<<"Done.\n";
             }else if(op=="buy")
             {
                 BookManager.buy(a);
+                std::cerr<<"Done.\n";
             }else if(op=="select")
             {
                 BookManager.select(a);
+                std::cerr<<"Done.\n";
             }else if(op=="modify")
             {
                 BookManager.modify(a);
+                std::cerr<<"Done.\n";
             }else if(op=="import")
             {
                 BookManager.import(a);
+                std::cerr<<"Done.\n";
             }else if(op=="log")
             {
                 std::cout<<"LOG:"<<'\n';
                 for(auto x: LOG) std::cout<<x.first<<": "<<x.second<<'\n';
+                std::cerr<<"Done.\n";
+            }else 
+            {
+                throw 0;
             }
             User cur=Stack.getTop();
             LOG.push_back(std::make_pair(cur.UserID,stcop));
