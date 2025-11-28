ee5edb02a076aead359c9cdb78048feb177911dc
WyEhNf <1716096609@qq.com>
2025-11-28 22:14:36 +0800

    “展开了hpp，用以提交”

--- src/FileStore.cpp
+++ src/FileStore.cpp
@@ -1,14 +1,147 @@
-#ifndef FILESTORE_HPP
-#define FILESTORE_HPP
-
-#include "Chars.hpp"
-#include "MemoryRiver.hpp"
 #include <cstring>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <vector>
 const int SIZE = 300;
+
+using std::fstream;
+using std::ifstream;
+using std::ofstream;
+using std::string;
+
+template <class T, int info_len = 2> class MemoryRiver {
+private:
+  fstream file;
+  string file_name;
+  int sizeofT = sizeof(T);
+
+public:
+  MemoryRiver() = default;
+
+  MemoryRiver(const string &file_name) : file_name(file_name) {}
+
+  ~MemoryRiver() { file.close(); }
+
+  void initialise(string FN = "") {
+    if (FN != "")
+      file_name = FN;
+    if (std::filesystem::exists(file_name)) {
+      file.open(file_name, std::ios::in | std::ios::out | std::ios::binary);
+      file.seekp(info_len * sizeof(int), std::ios_base::beg);
+      file.close();
+      file.open(file_name, std::ios::in | std::ios::out | std::ios::binary);
+      return;
+    }
+    file.open(file_name, std::ios::out | std::ios::binary);
+    int tmp = 0;
+    for (int i = 0; i < info_len; ++i)
+      file.write(reinterpret_cast<char *>(&tmp), sizeof(int));
+    file.close();
+    file.open(file_name, std::ios::in | std::ios::out | std::ios::binary);
+  }
+
+  // 读出第n个int的值赋给tmp，1_base
+  void get_info(int &tmp, int n) {
+    if (n > info_len)
+      return;
+    file.seekg((n - 1) * sizeof(int), std::ios_base::beg);
+    file.read(reinterpret_cast<char *>(&tmp), sizeof(int));
+  }
+
+  void write_info(int tmp, int n) {
+    if (n > info_len)
+      return;
+    file.seekp((n - 1) * sizeof(int), std::ios_base::beg);
+    file.write(reinterpret_cast<char *>(&tmp), sizeof(int));
+  }
+
+  // 在文件合适位置写入类对象t，并返回写入的位置索引index
+  // 位置索引意味着当输入正确的位置索引index，在以下三个函数中都能顺利的找到目标对象进行操作
+  // 位置索引index可以取为对象写入的起始位置
+  int write(T &t) {
+    file.seekp(0, std::ios::end);
+    int index = file.tellp();
+    file.write(reinterpret_cast<char *>(&t), sizeof(T));
+    return index;
+  }
+
+  // 用t的值更新位置索引index对应的对象，保证调用的index都是由write函数产生
+  void update(T &t, const int index) {
+    file.seekp(index, std::ios_base::beg);
+    file.write(reinterpret_cast<char *>(&t), sizeof(T));
+  }
+
+  // 读出位置索引index对应的T对象的值并赋值给t，保证调用的index都是由write函数产生
+  void read(T &t, const int index) {
+    file.seekg(index, std::ios_base::beg);
+    file.read(reinterpret_cast<char *>(&t), sizeof(T));
+  }
+
+  // 删除位置索引index对应的对象(不涉及空间回收时，可忽略此函数)，保证调用的index都是由write函数产生
+  void Delete(int index) { /* your code here */ }
+};
+
+struct chars {
+public:
+  char a[80];
+
+  chars() { a[0] = 0; }
+
+  chars(std::string &st) {
+    int len = st.length();        
+    if (len > 70) throw 0;
+    for (int i = 0; i < len; i++) a[i] = st[i];
+    a[len] = 0;
+  }
+
+  char &operator[](int id) { return a[id]; }
+
+  bool operator==(const chars other) const {
+    if (strlen(a) != strlen(other.a))
+      return 0;
+    for (int i = 0; i < strlen(a); i++)
+      if (a[i] != other.a[i]) return 0;
+      else if (!a[i])return 1;
+    return 1;
+  }
+  bool operator!=(const chars other) const { return !(*this == other); }
+  bool operator<(const chars other) const {
+    if (*this == other)
+      return 0;
+    int k = std::min(strlen(a), strlen(other.a)) + 1;
+    for (int i = 0; i < k; i++)
+      if (a[i] != other.a[i])
+        return a[i] < other.a[i];
+    return 0;
+  }
+  bool operator<=(const chars other) const {
+    return (*this == other) || (*this < other);
+  }
+  bool operator>(const chars other) const { return !(*this <= other); }
+  bool operator>=(const chars other) const { return !(*this < other); }
+};
+
+chars Chars(std::string st) {
+  int len = st.length();
+  chars res;
+  for (int i = 0; i < len; i++) res[i] = st[i];
+  for (int i = len; i < 61; i++) res[i] = 0;
+  return res;
+}
+
+std::string String(chars st) {
+  int len = strlen(st.a);
+  std::string res;
+  res.resize(len);
+  for (int i = 0; i < len; i++) res[i] = st.a[i];
+  return res;
+}
+
+std::ostream &operator<<(std::ostream &out, const chars &now) {
+  out << now.a;
+  return out;
+}
 
 template <typename indextype, typename valuetype> class FileStore {
   class data {
@@ -273,5 +406,3 @@ int main()
     }
     return 0;
 }
-
-#endif
