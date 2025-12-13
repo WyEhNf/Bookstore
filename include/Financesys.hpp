1be2eb4b5466b735f10e1ac3c69f40c58fe94a30
WyEhNf <1716096609@qq.com>
2025-12-13 17:20:22 +0800

    Finish Header-Source File Separation

--- include/Financesys.hpp
+++ include/Financesys.hpp
@@ -8,65 +8,27 @@
 #include "UserStack.hpp"
 #include <filesystem>
 #include <iostream>
+#include <vector>
+#include <string>
+
+using std::string;
+
+extern UserStack Stack;
 
 class Finance_Sys {
 private:
-  MemoryRiver<Finance, 1> FinanceList;
-  FileStore<int, Finance> FinanceStore;
-  int cnt;
-  friend class Manager;
+    MemoryRiver<Finance, 1> FinanceList;
+    FileStore<int, Finance> FinanceStore;
+    int cnt;
+    friend class Manager;
 
 public:
-  Finance_Sys() {
-    FinanceStore.Init("Finance_File");
-    if (std::filesystem::exists("Finance_File_tot")) {
-      FinanceList.initialise("Finance_File_tot");
-      FinanceList.get_info(cnt, 1);
-    } else
-      FinanceList.initialise("Finance_File_tot"), cnt = 0;
-  }
-  ~Finance_Sys() { FinanceList.write_info(cnt, 1); }
-  void update(Finance cur) {
-    if (!cnt) {
-      ++cnt;
-      FinanceStore.ins(cnt, cur);
-    } else {
-      std::vector<Finance> res = FinanceStore.qry(cnt);
-      cnt++;
-      res[0] = res[0] + cur;
-      FinanceStore.ins(cnt, res[0]);
-    }
-  }
-  void print(Read &a) {
-    User cur = Stack.getTop();
-    if (cur.Privilege < 7) {
-      throw 0;
-    }
-    Finance res = Finance();
-    if (a.length() == 0) {
-      if (cnt)
-        res = *(FinanceStore.qry(cnt).begin());
-      std::cout << res << '\n';
-    } else {
-      string Count_st = a.get_string();
-      if (!isInteger(Count_st)) throw 0;
-      int Count = string_to_int(Count_st);
-      if (a.length()) throw 0;
-      if (Count > cnt) throw 0;
-      if (Count < 0) throw 0;
-      if (Count == 0) {
-        std::cout << '\n';
-        return;
-      }
-      if (Count == cnt) {
-        std::cout << *(FinanceStore.qry(cnt).begin()) << '\n';
-        return;
-      }
-      Finance R = *(FinanceStore.qry(cnt).begin());
-      Finance L = *(FinanceStore.qry(cnt - Count).begin());
-      std::cout << (R - L) << '\n';
-    }
-  }
-} Finance_Record;
+    Finance_Sys();
+    ~Finance_Sys();
+    void update(Finance cur);
+    void print(Read &a);
+};
+
+extern Finance_Sys Finance_Record;
 
 #endif
--- include/UserStore.hpp
+++ include/UserStore.hpp
@@ -5,65 +5,31 @@
 #include "FileStore.hpp"
 #include "User.hpp"
 #include <vector>
+#include <string>
+
+using std::string;
 
 class UserStore {
 private:
-  FileStore<chars, User> UserList;
-  friend class UserStack;
-  friend class Finance;
-  friend class Manager;
+    FileStore<chars, User> UserList;
+    friend class UserStack;
+    friend class Finance;
+    friend class Manager;
 
 public:
-  UserStore() {
-    UserList.Init("UserList");
-    User su("root", "sjtu", "root", 7);
-    string id = "root";
-    std::vector<User> res = UserList.qry((chars)(id));
-    if (res.size() == 0)
-      UserList.ins(id, su);
-  }
+    UserStore();
 
-  bool inStore(string ID) {
-    std::vector<User> res = UserList.qry((chars)(ID));
-    if (res.size() == 0) return false;
-    return true;
-  }
+    bool inStore(string ID);
 
-  bool get_user(string ID, User &cur) {
-    std::vector<User> res = UserList.qry((chars)(ID));
-    if (res.size() == 0) return false;
-    cur = res[0];
-    return true;
-  }
+    bool get_user(string ID, User &cur);
 
-  void user_create(string ID, string passwd, string username, int pri) {
-    User cur(ID, passwd, username, pri);
-    if (inStore(ID)) throw 0;
-    UserList.ins(ID, cur);
-  }
+    void user_create(string ID, string passwd, string username, int pri);
 
-  void user_delete(string ID) {
-    if (!inStore(ID)) throw 0;
-    std::vector<User> res = UserList.qry((chars)(ID));
-    UserList.del((chars)(ID), res[0]);
-  }
+    void user_delete(string ID);
 
-  void change_password(string ID, string OldPassword, string CurPassword) {
-    if (!inStore(ID)) throw 0;
-    std::vector<User> res = UserList.qry((chars)(ID));
-    if (res[0].password != OldPassword) throw 0;
-    UserList.del(ID, res[0]);
-    res[0].password = CurPassword;
-    UserList.ins(ID, res[0]);
-  }
-  
-  void change_password(string ID, string CurPassword) {
-    if (!inStore(ID)) throw 0;
-    std::vector<User> res = UserList.qry((chars)(ID));
-    UserList.del(ID, res[0]);
-    res[0].password = CurPassword;
-    UserList.ins(ID, res[0]);
-  }
+    void change_password(string ID, string OldPassword, string CurPassword);
+    
+    void change_password(string ID, string CurPassword);
 };
 
 #endif
